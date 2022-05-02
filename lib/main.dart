f1fa58257dca280bc7b7bc651b0d9187bef1b461
Byron Kimani <byronkimani@gmail.com>
2022-05-02 16:18:44 +0300

    chore: restrict application to portrait mode only
    - restrict application to portrait mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:designly/business_logic/core/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   BlocOverrides.runZoned(
-    () => runApp(AppWidget()),
+    () => SystemChrome.setPreferredOrientations(
+      <DeviceOrientation>[DeviceOrientation.portraitUp],
+    ).then((_) {
+      runApp(AppWidget());
+    }),
   );
 }
